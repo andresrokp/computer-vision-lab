a52bc61aed2c6d7153c4af0cf940702d654cb8f0
andres frp <afrokap@gmail.com>
2022-03-01 17:38:55 -0500

    low res, better speed

--- my-craft/aerocam-opp/yoloCam.py
+++ my-craft/aerocam-opp/yoloCam.py
@@ -4,6 +4,31 @@ import math
 
 print("-\n--\n---\n----\n-----\n----\n---\n--\n-")
 
+
+
+def modo1():
+    
+    classId, confs, boxes = cnn.detect(frame, confThreshold=0.3)
+    print(type(classId))
+    # if len(classId()) is not 0:
+    detectResult = zip(classId.flatten(), confs.flatten(),boxes)
+    print(detectResult)
+
+    for classId_x, conf_x, box_x in detectResult:
+        print (classId_x, conf_x, box_x)
+        cv2.rectangle(frame,box_x,color=(0,255,0),thickness=2)
+        cv2.putText(frame
+                    ,f'{CocoClasses[classId_x-1]} @ {conf_x}'
+                    ,(box_x[0], box_x[1]-10)
+                    , cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0,255,0), 2
+                    );
+    
+    cv2.imshow("sec cam video", frame)
+
+# def modo2():
+#     if key == ord('q'):
+#         cv2.writeOpticalFlow
+
 CocoClasses = ['person','bicycle','car','motorbike','aeroplane','bus','train',
             'truck','boat','traffic light','fire hydrant','stop sign',
             'parking meter','bench','bird','cat','dog','horse','sheep','cow',
@@ -21,10 +46,11 @@ CocoClasses = ['person','bicycle','car','motorbike','aeroplane','bus','train',
 video = cv2.VideoCapture()
 video.open(myvars.url)
 _,capture = video.read()
-capture = cv2.resize(capture, (0,0), fx=0.4, fy=0.4)
+capture = cv2.resize(capture, (0,0), fx=0.2, fy=0.2)
 width = capture.shape[0]
 height = capture.shape[1]
 frame = capture
+
 
 cnn = cv2.dnn_DetectionModel(myvars.weights, myvars.config);
 cnn.setInputSize(width, height);
@@ -32,30 +58,25 @@ cnn.setInputScale(1.0/127.5);
 cnn.setInputMean(120);
 cnn.setInputSwapRB(True)
 
+ssc = 0
+
+ssc += 1;
+cv2.imwrite(f"screenshots/screenshot{ssc}.png", capture)
+
 while True:
     _, frame = video.read()
-    frame = cv2.resize(frame, (0,0), fx=0.4, fy=0.4)
-    # frame = frame[math.floor(0.1*width):math.floor(0.8*width), math.floor(0.1*height):math.floor(0.7*height)]
-    cv2.imshow("sec cam video", frame)
+    frame = cv2.resize(frame, (0,0), fx=0.2, fy=0.2)
+    frame = frame[math.floor(0.1*width):math.floor(0.8*width), math.floor(0.1*height):math.floor(0.7*height)]
     
-    classId, confs, boxes = cnn.detect(capture, confThreshold=0.3)
-    print(type(classId))
-    # if len(classId()) is not 0:
-    detectResult = zip(classId.flatten(), confs.flatten(),boxes)
-    print(detectResult)
-
-    for classId_x, conf_x, box_x in detectResult:
-        print (classId_x, conf_x, box_x)
-        cv2.rectangle(capture,box_x,color=(0,255,0),thickness=2)
-        cv2.putText(frame
-                    ,f'{CocoClasses[classId_x-1]} @ {conf_x}'
-                    ,(box_x[0], box_x[1]-10)
-                    , cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0,255,0), 2
-                    );
-
+    modo1()
     key = cv2.waitKey(1)
+    
     if key == ord('q'):
         break
 
+    
+
+
 video.release()
 cv2.destroyAllWindows()
+
