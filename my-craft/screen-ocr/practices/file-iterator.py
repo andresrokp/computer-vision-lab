d98246666c91e1b0fa2abbf7a347e377ec24c97c
andres frp <afrokap@gmail.com>
2022-03-02 13:13:38 -0500

    slicing[] to take all general status region

--- my-craft/screen-ocr/practices/file-iterator.py
+++ my-craft/screen-ocr/practices/file-iterator.py
@@ -1,8 +1,8 @@
 import os
+from turtle import width
 import myvars
 import cv2
-import time
-
+import math
 
 print("-\n--\n---\n----\n-----\n----\n---\n--\n-")
 
@@ -13,6 +13,11 @@ for file in os.listdir(path):
      filename = os.fsdecode(file)
      print('filename: ',filename)
      pic = cv2.imread(f'{myvars.picspath}{filename}')
-     cv2.imshow(f'{filename}', pic)
+     height = pic.shape[0]
+     width = pic.shape[1]
+     print('width ',width, ' - height ',height)
+     genStatus = pic[10:85, 935:1365]
+     cv2.imshow(f'{filename}', genStatus)
+     # break
 
 key = cv2.waitKey(0)
