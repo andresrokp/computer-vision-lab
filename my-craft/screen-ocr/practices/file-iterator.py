9472cf0b0bb5b2e873313dd2211bc1941633163d
andres frp <afrokap@gmail.com>
2022-03-03 18:33:53 -0500

    init match-case for screen num

--- my-craft/screen-ocr/practices/file-iterator.py
+++ my-craft/screen-ocr/practices/file-iterator.py
@@ -8,6 +8,7 @@ import pytesseract as pts
 
 print("-\n--\n---\n----\n-----\n----\n---\n--\n-")
 
+
 path = os.fsencode(mv.picspath)
 print(f'path: {path}')
 pts.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
@@ -15,18 +16,29 @@ pts.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract.
     
 for file in os.listdir(path):
      filename = os.fsdecode(file)
-     print('filename: ',filename)
+     print('\n> filename: ',filename)
      
      wholeScreen = cv2.imread(f'{mv.picspath}{filename}')
      resized = cv2.resize(wholeScreen, (0,0), fx=0.5, fy=0.5)
      # cv2.imshow('resized', resized)
      
      chunkScreenNumber = wholeScreen[mv.screenNumRange]
-     numScreenNumber = pts.image_to_string(chunkScreenNumber)
-     print(numScreenNumber)
-     # cv2.imshow(f'cant ciclos {filename}', chunkScreenNumber)
-     
+     gray = cv2.cvtColor(chunkScreenNumber, cv2.COLOR_BGR2GRAY)
+     blur = cv2.GaussianBlur(gray, (3, 3), 0)
+     # thresh = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY)[1]
+     thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
+     numScreenNumber = pts.image_to_string(chunkScreenNumber).strip()
+     print(numScreenNumber)   
+     cv2.imshow(f'cant ciclos {filename}', thresh)
 
+     match numScreenNumber:
+          case '1103':
+               print('Caleffacción filas longitudinales')
+          case '2003':
+               print('Vista general presión')
+          case _:
+               print('OTRA PANTALLA')
+     
      # break
 
 key = cv2.waitKey(0)
