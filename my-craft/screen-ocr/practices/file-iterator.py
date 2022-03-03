725bf59094c53c2e3bf3cc7ddd4503f9853262a2
andres frp <afrokap@gmail.com>
2022-03-03 08:45:18 -0500

    getting all screen number chuncks . paint + index_exp[]

--- my-craft/screen-ocr/practices/file-iterator.py
+++ my-craft/screen-ocr/practices/file-iterator.py
@@ -1,30 +1,27 @@
 import os
 from turtle import width
-import myvars
+import myvars as mv
 import cv2
 import math
 import matplotlib.pyplot as plt
 
 print("-\n--\n---\n----\n-----\n----\n---\n--\n-")
 
-path = os.fsencode(myvars.picspath)
+path = os.fsencode(mv.picspath)
 print(f'path: {path}')
 
     
 for file in os.listdir(path):
      filename = os.fsdecode(file)
      print('filename: ',filename)
-     pic = cv2.imread(f'{myvars.picspath}{filename}')
-     resized = cv2.resize(pic, (0,0), fx=0.3, fy=0.3)
-     cv2.imshow('resized', resized)
-     height = pic.shape[0]
-     width = pic.shape[1]
-     print('width ',width, ' - height ',height)
-     genStatus = pic[10:85, 935:1365]
-     cv2.namedWindow(f'gen status {filename}', flags=cv2.WINDOW_GUI_EXPANDED)
-     cv2.imshow(f'gen status {filename}', genStatus)
-     cantCiclos = genStatus[30:50, 130:207]
-     cv2.imshow(f'cant ciclos {filename}', cantCiclos)
-     break
+     
+     wholeScreen = cv2.imread(f'{mv.picspath}{filename}')
+     resized = cv2.resize(wholeScreen, (0,0), fx=0.5, fy=0.5)
+     # cv2.imshow('resized', resized)
+     
+     chunkScreenNumber = wholeScreen[mv.screenNumRange]
+     
+     cv2.imshow(f'cant ciclos {filename}', chunkScreenNumber)
+     # break
 
 key = cv2.waitKey(0)
