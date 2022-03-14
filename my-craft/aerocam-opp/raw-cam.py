e0a463bcb762f8d0702474141f30f0fda7863cd4
andres frp <afrokap@gmail.com>
2022-03-14 16:12:12 -0500

    init script : images spliter

--- my-craft/aerocam-opp/raw-cam.py
+++ my-craft/aerocam-opp/raw-cam.py
@@ -3,19 +3,20 @@ import myvars
 import datetime
 
 video = cv2.VideoCapture()
-video.open(myvars.url)
+video.open(myvars.url1)
 
 dateAndTime = datetime.datetime.now().strftime("CAP-%d%m%Y-%H-%M-%S")
 print(dateAndTime)
 
+counter = 0;
 while True:
     _,frame = video.read();
 
-    print(frame.shape)
     frame = cv2.resize(frame, (0,0), fx=0.4, fy=0.4)
     cv2.imshow("capture termoformado", frame)
     dateAndTime = datetime.datetime.now().strftime("CAP-%d%m%Y-%H-%M-%S")
-    print(dateAndTime)
+    counter+=1
+    print(counter)
     key = cv2.waitKey(1);
     if key == ord('q'):
         break
