bfbe1c3ff0e69b682bc8a4878dffb655ee71909b
andres frp <afrokap@gmail.com>
2022-03-14 17:23:11 -0500

    string simulation file spliting - enumerate

--- yolo-training-colab/desarrollo-1/images/img_splitter.py
+++ yolo-training-colab/desarrollo-1/images/img_splitter.py
@@ -6,8 +6,8 @@ import random
 print("-\n--\n---\n----\n-----\n----\n---\n--\n-\n")
 
 inputDirName = 'oxxo_images_renamed'
-trainRatio = 0.85
-validationRatio = 0.15
+trainRatio = 0.80
+validRatio = 0.15
 outputDirName = 'datasets'
 
 
@@ -17,14 +17,18 @@ else:
     shutil.rmtree(outputDirName)
 
 fileList = os.listdir(inputDirName)
-print(len(fileList))
+filesQnt = len(fileList)
 
-if len(fileList) == 0:
+if filesQnt == 0:
     print(f'{inputDirName}/  is empty')
 
 trainDir = f'{outputDirName}/train'
 validDir = f'{outputDirName}/valid'
 testDir = f'{outputDirName}/test'
+
+trainCounter = 0
+validCounter = 0
+testCounter = 0
 
 if not os.path.exists(trainDir):
     os.makedirs(trainDir)
@@ -34,6 +38,28 @@ if not os.path.exists(validDir):
 
 if not os.path.exists(testDir):
     os.makedirs(testDir)
+
+
+random.shuffle(fileList)
+
+for idx, fileName in enumerate(fileList):
+    if fileName.endswith('.jpg'):
+        relPosition = idx/filesQnt
+        print(idx,' ',filesQnt,' ',relPosition)
+        if relPosition<trainRatio:
+            print('Train')
+            trainCounter += 1;
+        if trainRatio <= relPosition < (trainRatio+validRatio):
+            print('Validation')
+            validCounter += 1;
+        if (trainRatio+validRatio) <= relPosition:
+            print('Test')
+            testCounter += 1;
+
+print('\n----\n')
+print(f'trainCounter = {trainCounter}')
+print(f'validCounter = {validCounter}')
+print(f'testCounter = {testCounter}')
 
 
 
