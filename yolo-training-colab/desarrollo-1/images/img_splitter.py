25f1d8881f0e6119f15c2eb85a41b4b8b1e40456
andres frp <afrokap@gmail.com>
2022-03-14 19:04:27 -0500

    file spliting OK - split '.' copyfile

--- yolo-training-colab/desarrollo-1/images/img_splitter.py
+++ yolo-training-colab/desarrollo-1/images/img_splitter.py
@@ -6,8 +6,8 @@ import random
 print("-\n--\n---\n----\n-----\n----\n---\n--\n-\n")
 
 inputDirName = 'oxxo_images_renamed'
-trainRatio = 0.80
-validRatio = 0.15
+trainRatio = 0.85
+validRatio = 0.10
 outputDirName = 'datasets'
 
 
@@ -29,6 +29,7 @@ testDir = f'{outputDirName}/test'
 trainCounter = 0
 validCounter = 0
 testCounter = 0
+allImgCounter = 1
 
 if not os.path.exists(trainDir):
     os.makedirs(trainDir)
@@ -42,24 +43,35 @@ if not os.path.exists(testDir):
 
 random.shuffle(fileList)
 
-for idx, fileName in enumerate(fileList):
+for fileName in fileList:
+    print(fileName, end=' ')
     if fileName.endswith('.jpg'):
-        relPosition = idx/filesQnt
-        print(idx,' ',filesQnt,' ',relPosition)
+        relPosition = allImgCounter/((filesQnt-1)/2)  # afr :: keep an aye on the -1 is for the classes.txt file
+        print(allImgCounter,' ',relPosition)
+        justTheName = fileName.split('.')[0]
         if relPosition<trainRatio:
             print('Train')
+            copyfile(os.path.join(inputDirName,justTheName+'.jpg'),os.path.join(trainDir,justTheName+'.jpg'))
+            copyfile(os.path.join(inputDirName,justTheName+'.txt'),os.path.join(trainDir,justTheName+'.txt'))
             trainCounter += 1;
         if trainRatio <= relPosition < (trainRatio+validRatio):
             print('Validation')
+            copyfile(os.path.join(inputDirName,justTheName+'.jpg'),os.path.join(validDir,justTheName+'.jpg'))
+            copyfile(os.path.join(inputDirName,justTheName+'.txt'),os.path.join(validDir,justTheName+'.txt'))
             validCounter += 1;
         if (trainRatio+validRatio) <= relPosition:
             print('Test')
+            copyfile(os.path.join(inputDirName,justTheName+'.jpg'),os.path.join(testDir,justTheName+'.jpg'))
+            copyfile(os.path.join(inputDirName,justTheName+'.txt'),os.path.join(testDir,justTheName+'.txt'))
             testCounter += 1;
+        allImgCounter += 1;
 
 print('\n----\n')
 print(f'trainCounter = {trainCounter}')
 print(f'validCounter = {validCounter}')
 print(f'testCounter = {testCounter}')
+
+
 
 
 
