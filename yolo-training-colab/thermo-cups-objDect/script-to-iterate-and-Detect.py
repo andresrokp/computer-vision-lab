63b8044dfbc1a4ba779959f85b98332ed326f8de
andres frp <afrokap@gmail.com>
2022-06-17 09:08:10 -0500

    termoYolo : iterating imgs folder and saving detection pictures

--- yolo-training-colab/thermo-cups-objDect/script-to-iterate-and-Detect.py
+++ yolo-training-colab/thermo-cups-objDect/script-to-iterate-and-Detect.py
@@ -4,6 +4,7 @@ import os
 
 print("-\n--\n---\n----\n-----\n------\n-------\n--------\n---------\n----------\n-----------\n------------\n-------------\n--------------\n-------------\n------------\n-----------\n----------\n---------\n--------\n-------\n------\n-----\n----\n---\n--\n-")
 
+# Declaración de nombres propios // Input variables declaration
 classesList = ['monoTapon'];
 configFilePath = f'{myvars.resultsPath}/custom-yolov4-detector.cfg';
 weightsFilePath = f'{myvars.resultsPath}/custom-yolov4-detector_best.weights';
@@ -13,37 +14,35 @@ imgFileNamesList = os.listdir(imgsFilesPath)[0:-1];
 
 print(imgFileNamesList);
 
-img = cv2.imread(f'{imgsFilesPath}/CAP-11032022-19-07-17.jpg')
+for imgName in imgFileNamesList:
 
-print(img.shape)
-cnn = cv2.dnn_DetectionModel(weightsFilePath, configFilePath);
-print(cnn)
+    img = cv2.imread(f'{imgsFilesPath}/{imgName}')
 
-cnn.setInputSize(447, 797);
-cnn.setInputScale(1.0/127.5);
-cnn.setInputMean(1);
-cnn.setInputSwapRB(True)
+    # Config de la Red Neur Conv // Conv Neural Net set up
+    cnn = cv2.dnn_DetectionModel(weightsFilePath, configFilePath);
+    cnn.setInputSize(447, 797);
+    cnn.setInputScale(1.0/127.5);
+    cnn.setInputMean(1);
+    cnn.setInputSwapRB(True)
 
-classId, confs, boxes = cnn.detect(img, confThreshold=0.3)
+    # Lanzamiento de la detección // running detection
+    classId, confs, boxes = cnn.detect(img, confThreshold=0.3)
 
-print(len(classId))
+    if len(classId) != 0: # evitando nulls // avoiding nulls
+        detectResult = zip(classId.flatten(), confs.flatten(),boxes)
+        
+        # Iterando sobre cada resultado // iteratin over each detection
+        for classId_x, conf_x, box_x in detectResult:
+            print (classId_x, conf_x, box_x)
+            cv2.rectangle(img,box_x,color=(0,255,0),thickness=2)
+            cv2.putText(img
+                        ,f'{classesList[classId_x-1]} @ {conf_x}'
+                        ,(box_x[0], box_x[1]-10)
+                        , cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0,255,0), 2
+                        );
+    else:
+        print("\nx x x NO SE ENCONTRÓ OBJETO x x x\n\n")
 
-if len(classId) != 0:
-    detectResult = zip(classId.flatten(), confs.flatten(),boxes)
-    print(detectResult)
-
-
-    for classId_x, conf_x, box_x in detectResult:
-        print (classId_x, conf_x, box_x)
-        cv2.rectangle(img,box_x,color=(0,255,0),thickness=2)
-        cv2.putText(img
-                    ,f'{classesList[classId_x-1]} @ {conf_x}'
-                    ,(box_x[0], box_x[1]-10)
-                    , cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0,255,0), 2
-                    );
-else:
-    print("\nx x x NO SE ENCONTRÓ OBJETO x x x\n\n")
-
-cv2.imshow("the image", img)
+    cv2.imwrite(f'termoYoloDetections/{imgName}', img)
 key = cv2.waitKey(0)
 
